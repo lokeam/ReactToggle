50ade249f798f2c44114f5e1a2b096903c71acdb
Ahnming Loke <lokeahnming@gmail.com>
2018-07-14 20:18:35 -0400

    refactor: replace extraneous div with React Fragment

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Toggle from './ToggleRPC.js';
 
 export default class App extends Component {
@@ -8,10 +8,10 @@ export default class App extends Component {
         <h1>Toggle Component Sample</h1>
           <Toggle>
             {({on, toggle}) => (
-              <div>
+              <Fragment>
                 {on && <h1>Show Me</h1>}
                 <button onClick={toggle}>Show / Hide</button>
-              </div>
+              </Fragment>
             )}
           </Toggle>
       </div>
